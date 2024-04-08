53b63d7d93f18784cda06b2a45f18416bda1066d
Liam Watts <elnewatts@gmail.com>
2024-04-08 14:54:31 +0100

    - Added scroll indicator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import Linkedin from '$lib/icons/Linkedin.svelte';
 	import HeroShapes from '$lib/HeroShapes.svelte';
 	import { gsap } from 'gsap';
+	import Scroll from '$lib/Scroll.svelte';
 
 	onMount(() => {
 		const t1 = gsap.timeline();
@@ -14,14 +15,20 @@
 </script>
 
 <div>
-	<div class="mb-12">
-		<div class="flex justify-between my-32 items-center">
+	<div class="mb-32">
+		<div class="flex md:justify-between my-32 items-center justify-center">
 			<div id="hero-content" class="text-center md:text-left px-4 md:px-0">
 				<h1 class="uppercase text-5xl text-primary leading-normal">
 					Creating Unique <br />Experiences
 				</h1>
+				<div class="flex mt-5 items-center justify-center md:justify-start">
+					<Scroll />
+					<p class="tracking-widest text-sm uppercase px-5 text-primary">Scroll for more!</p>
+				</div>
 			</div>
-			<HeroShapes />
+			<div class="hidden md:block">
+				<HeroShapes />
+			</div>
 		</div>
 
 		<div class="flex items-center w-full justify-between px-4 md:px-0">
@@ -31,6 +38,5 @@
 		</div>
 	</div>
 
-	<div class=""></div>
 	<ProjectCard />
 </div>
