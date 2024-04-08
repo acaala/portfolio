1422cbf530b47be8d584ea6e8a305eadbfe8dcc5
Liam Watts <elnewatts@gmail.com>
2024-04-08 14:15:15 +0100

    - Addeed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#4cfcd3',
+				secondary: '#fd49a0',
+				tertiary: '#7b3ccc'
+			}
+		}
+	},
+	plugins: []
+};
