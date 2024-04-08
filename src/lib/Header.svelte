7b294059d989e8376f12d6a1c787a7ccb4acb38d
Liam Watts <elnewatts@gmail.com>
2024-04-08 14:16:06 +0100

    - Basic page layout and styling

---
+++ src/lib/Header.svelte
@@ -0,0 +1,3 @@
+<header class="my-4">
+	<h2 class="title-font text-3xl text-primary">Liam Watts</h2>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,24 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import ProjectCard from '$lib/ProjectCard.svelte';
+	import Github from '$lib/icons/Github.svelte';
+	import Linkedin from '$lib/icons/Linkedin.svelte';
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<div>
+	<div class="mb-12">
+		<div class="text-center md:text-left my-32 px-4 md:px-0">
+			<h1 class="uppercase text-5xl text-primary leading-normal">
+				Creating Unique <br />Experiences
+			</h1>
+		</div>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+		<div class="flex items-center w-full justify-between px-4 md:px-0">
+			<div class="divider h-0.5 bg-gray-200 w-3/4"></div>
+			<a href="https://github.com/acaala"><Github /></a>
+			<a href="https://www.linkedin.com/in/lawatts1/"><Linkedin /></a>
+		</div>
+	</div>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+	<div class=""></div>
+	<ProjectCard />
+</div>
