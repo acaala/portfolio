e484c4e423668558a17c6352defe53fabff4b2e5
Liam Watts <elnewatts@gmail.com>
2025-11-30 15:16:25 +0100

    - Fix header spacing mobile

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,5 @@
 <header class="my-4">
-	<div class="text-primary flex w-2/3 items-center justify-between">
+	<div class="text-primary flex items-center justify-between lg:w-2/3">
 		<h2 class="title-font text-3xl">Liam Watts</h2>
 		<a href="#clients" class="text-sm tracking-widest uppercase">clients</a>
 		<a href="#projects" class="text-sm tracking-widest uppercase">projects</a>
